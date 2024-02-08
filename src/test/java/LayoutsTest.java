b974c759ce573f7f2e375d9c12387058f235b437
ElenaBykanova <ogata@inbox.ru>
2024-02-08 09:45:27 +0300

    Добавлен шаг moveToElement Клик по другому селектору

--- src/test/java/LayoutsTest.java
+++ src/test/java/LayoutsTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -39,13 +40,14 @@ public class LayoutsTest {
         driver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
         logger.info("Открыт сайт demo.w3layouts в режиме киоска");
 
-
         List<WebElement> listOfImg = driver.findElements(By.cssSelector("div.content-overlay"));
 
         JavascriptExecutor js = (JavascriptExecutor)driver;
         js.executeScript("arguments[0].scrollIntoView()", listOfImg.get(0));
 
-        listOfImg.get(0).click();
+        new Actions(driver).moveToElement(listOfImg.get(0)).build().perform();
+
+        driver.findElements(By.cssSelector("a.image-zoom")).get(0).click();
         logger.info("Кликнули по картинке");
 
         String modalSelector = ".pp_hoverContainer";
